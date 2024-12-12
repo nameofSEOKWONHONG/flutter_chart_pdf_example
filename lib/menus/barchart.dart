a3982af56f1481f270ca6785232e505b3be34481
seokwon <h20913@gmail.com>
2024-12-12 20:47:39 +0900

    update

--- lib/menus/barchart.dart
+++ lib/menus/barchart.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
@@ -12,16 +13,17 @@ class BarChartPage extends StatelessWidget {
 
   final GlobalKey _repaintKey = GlobalKey();
 
-
   final pilateColor = Colors.purple;
   final cyclingColor = Colors.cyan;
   final quickWorkoutColor = Colors.blueAccent;
   final betweenSpace = 0.2;
 
-  BarChartGroupData generateGroupData(int x,
+  BarChartGroupData generateGroupData(
+      int x,
       double pilates,
       double quickWorkout,
-      double cycling,) {
+      double cycling,
+      ) {
     return BarChartGroupData(
       x: x,
       groupVertically: true,
@@ -30,63 +32,63 @@ class BarChartPage extends StatelessWidget {
           fromY: 0,
           toY: pilates,
           color: pilateColor,
-          width: 5,
+          width: 2,
         ),
         BarChartRodData(
           fromY: pilates + betweenSpace,
           toY: pilates + betweenSpace + quickWorkout,
           color: quickWorkoutColor,
-          width: 5,
+          width: 2,
         ),
         BarChartRodData(
           fromY: pilates + betweenSpace + quickWorkout + betweenSpace,
           toY: pilates + betweenSpace + quickWorkout + betweenSpace + cycling,
           color: cyclingColor,
-          width: 5,
+          width: 2,
         ),
       ],
     );
   }
 
   Widget bottomTitles(double value, TitleMeta meta) {
-    const style = TextStyle(fontSize: 10);
+    const style = TextStyle(fontSize: 5);
     String text;
     switch (value.toInt()) {
       case 0:
-        text = 'JAN';
+        text = 'PM10';
         break;
       case 1:
-        text = 'FEB';
+        text = '11';
         break;
       case 2:
-        text = 'MAR';
+        text = '12';
         break;
       case 3:
-        text = 'APR';
+        text = 'AM1';
         break;
       case 4:
-        text = 'MAY';
+        text = '2';
         break;
       case 5:
-        text = 'JUN';
+        text = '3';
         break;
       case 6:
-        text = 'JUL';
+        text = '4';
         break;
       case 7:
-        text = 'AUG';
+        text = '5';
         break;
       case 8:
-        text = 'SEP';
+        text = '6';
         break;
       case 9:
-        text = 'OCT';
+        text = '7';
         break;
       case 10:
-        text = 'NOV';
+        text = '8';
         break;
       case 11:
-        text = 'DEC';
+        text = '9';
         break;
       default:
         text = '';
@@ -99,36 +101,145 @@ class BarChartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size; // 화면 크기
+    final screenWidth = size.width;
+    final screenHeight = size.height;
+
     return RepaintBoundary(
       key: _repaintKey,
-      child: Padding(
-        padding: const EdgeInsets.all(24),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ElevatedButton(
-                onPressed: () => _exportToPDF(context),
-                child: const Text("PDF")
+      child: Stack(
+        children: [
+          ElevatedButton(
+            onPressed: () => _exportToPDF(context),
+            child: const Text("PDF"),
+          ),
+          // SVG를 전체 화면에 맞추기
+          Positioned.fill(
+            child: SvgPicture.asset(
+              'assets/report.svg',
+              fit: BoxFit.contain,
             ),
-            const Text(
-              'Activity',
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+          ),
+          Positioned(
+              top: (screenHeight * 0.2),
+              left: (screenWidth * 0.62),
+              child: const Text(
+                '이지은',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 6,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+          ),
+          Positioned(
+              top: (screenHeight * 0.2),
+              left: (screenWidth * 0.74),
+              child: const Text(
+                '여성',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 6,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+          ),
+          Positioned(
+              top: (screenHeight * 0.2),
+              left: (screenWidth * 0.84),
+              child: Text(
+                '1990.03.12',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 6,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+          ),
+          Positioned(
+              top: (screenHeight * 0.218),
+              left: (screenWidth * 0.62),
+              child: Text(
+                '2024년 9월 23일 오후 10시 - 9월 24일 오전 6시',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 5,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+          ),
+          Positioned(
+            top: (screenHeight * 0.456),
+            left: (screenWidth * 0.2),
+            width: (screenWidth * 0.44),
+            height: (screenHeight * 0.06),
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: BarChart(
+                BarChartData(
+                  alignment: BarChartAlignment.spaceBetween,
+                  titlesData: FlTitlesData(
+                    leftTitles: const AxisTitles(),
+                    rightTitles: const AxisTitles(),
+                    topTitles: const AxisTitles(),
+                    bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        getTitlesWidget: bottomTitles,
+                        reservedSize: 22,
+                      ),
+                    ),
+                  ),
+                  barTouchData: BarTouchData(enabled: false),
+                  borderData: FlBorderData(show: false),
+                  gridData: const FlGridData(show: false),
+                  barGroups: [
+                    generateGroupData(0, 2, 3, 2),
+                    generateGroupData(1, 2, 5, 1.7),
+                    generateGroupData(2, 1.3, 3.1, 2.8),
+                    generateGroupData(3, 3.1, 4, 3.1),
+                    generateGroupData(4, 0.8, 3.3, 3.4),
+                    generateGroupData(5, 2, 5.6, 1.8),
+                    generateGroupData(6, 1.3, 3.2, 2),
+                    generateGroupData(7, 2.3, 3.2, 3),
+                    generateGroupData(8, 2, 4.8, 2.5),
+                    generateGroupData(9, 1.2, 3.2, 2.5),
+                    generateGroupData(10, 1, 4.8, 3),
+                    generateGroupData(11, 2, 4.4, 2.8),
+                  ],
+                  maxY: 11 + (betweenSpace * 3),
+                  extraLinesData: ExtraLinesData(
+                    horizontalLines: [
+                      HorizontalLine(
+                        y: 3.3,
+                        color: pilateColor,
+                        strokeWidth: 1,
+                        dashArray: [20, 4],
+                      ),
+                      HorizontalLine(
+                        y: 8,
+                        color: quickWorkoutColor,
+                        strokeWidth: 1,
+                        dashArray: [20, 4],
+                      ),
+                      HorizontalLine(
+                        y: 11,
+                        color: cyclingColor,
+                        strokeWidth: 1,
+                        dashArray: [20, 4],
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-            const SizedBox(height: 8),
-            // LegendsListWidget(
-            //   legends: [
-            //     Legend('Pilates', pilateColor),
-            //     Legend('Quick workouts', quickWorkoutColor),
-            //     Legend('Cycling', cyclingColor),
-            //   ],
-            // ),
-            const SizedBox(height: 14),
-            AspectRatio(
+          ),
+          Positioned(
+            top: (screenHeight * 0.509),
+            left: (screenWidth * 0.2),
+            width: (screenWidth * 0.44),
+            height: (screenHeight * 0.06),
+            child: AspectRatio(
               aspectRatio: 2,
               child: BarChart(
                 BarChartData(
@@ -188,9 +299,75 @@ class BarChartPage extends StatelessWidget {
                 ),
               ),
             ),
-          ],
-        ),
-      )
+          ),
+          Positioned(
+            top: (screenHeight * 0.562),
+            left: (screenWidth * 0.2),
+            width: (screenWidth * 0.44),
+            height: (screenHeight * 0.06),
+            child: AspectRatio(
+              aspectRatio: 2,
+              child: BarChart(
+                BarChartData(
+                  alignment: BarChartAlignment.spaceBetween,
+                  titlesData: FlTitlesData(
+                    leftTitles: const AxisTitles(),
+                    rightTitles: const AxisTitles(),
+                    topTitles: const AxisTitles(),
+                    bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        getTitlesWidget: bottomTitles,
+                        reservedSize: 20,
+                      ),
+                    ),
+                  ),
+                  barTouchData: BarTouchData(enabled: false),
+                  borderData: FlBorderData(show: false),
+                  gridData: const FlGridData(show: false),
+                  barGroups: [
+                    generateGroupData(0, 2, 3, 2),
+                    generateGroupData(1, 2, 5, 1.7),
+                    generateGroupData(2, 1.3, 3.1, 2.8),
+                    generateGroupData(3, 3.1, 4, 3.1),
+                    generateGroupData(4, 0.8, 3.3, 3.4),
+                    generateGroupData(5, 2, 5.6, 1.8),
+                    generateGroupData(6, 1.3, 3.2, 2),
+                    generateGroupData(7, 2.3, 3.2, 3),
+                    generateGroupData(8, 2, 4.8, 2.5),
+                    generateGroupData(9, 1.2, 3.2, 2.5),
+                    generateGroupData(10, 1, 4.8, 3),
+                    generateGroupData(11, 2, 4.4, 2.8),
+                  ],
+                  maxY: 11 + (betweenSpace * 3),
+                  extraLinesData: ExtraLinesData(
+                    horizontalLines: [
+                      HorizontalLine(
+                        y: 3.3,
+                        color: pilateColor,
+                        strokeWidth: 1,
+                        dashArray: [20, 4],
+                      ),
+                      HorizontalLine(
+                        y: 8,
+                        color: quickWorkoutColor,
+                        strokeWidth: 1,
+                        dashArray: [20, 4],
+                      ),
+                      HorizontalLine(
+                        y: 11,
+                        color: cyclingColor,
+                        strokeWidth: 1,
+                        dashArray: [20, 4],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
